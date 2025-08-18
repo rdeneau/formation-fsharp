657c88fbd39f0bb0464c4da40bc76559bd2b83f2
Romain Deneau <rdeneau@d-edge.com>
2025-08-18 16:40:20 +0200

    test: 🧪 ValidationTests

---
+++ src/FSharpTraining/ComputationExpressions/ValidationTests.fs
@@ -0,0 +1,31 @@
+﻿module FSharpTraining.ValidationTests
+
+open FsToolkit.ErrorHandling
+open Swensen.Unquote
+open Xunit
+
+[<Theory>]
+[<InlineData("✅", 11)>]
+[<InlineData("❌", 0)>]
+let ``Desugar let! ... and!`` (_, expected) =
+    test
+        <@ validation {
+            let! x = Validation.ok 1
+            and! y = Validation.ok 10
+            return x + y
+        } = Validation.ok expected @>
+
+    let desugarValidation =
+        validation.Run(
+            validation.Delay(fun () ->
+                validation.BindReturn(
+                    validation.MergeSources( // ↩
+                        validation.Source(Validation.ok 1),
+                        validation.Source(Validation.ok 10)
+                    ),
+                    (fun (x, y) -> x + y)
+                )
+            )
+        )
+
+    desugarValidation =! Validation.ok expected
